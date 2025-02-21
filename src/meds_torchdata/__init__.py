67fce1edf3bd01fbf226a482fcc9333335d8220e
Matthew McDermott <mattmcdermott8@gmail.com>
2025-02-21 15:44:05 -0500

    Removed some other files; decided to simplify to remove the lightning dependency.

--- src/meds_torchdata/__init__.py
+++ src/meds_torchdata/__init__.py
@@ -1,7 +1,9 @@
 from importlib.metadata import PackageNotFoundError, version
 
+__package_name__ = "meds-torchdata"
+
 try:
-    __version__ = version("meds-torch")
+    __version__ = version(__package_name__)
 except PackageNotFoundError:
     # package is not installed
     __version__ = "unknown"

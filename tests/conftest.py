a28356c8005c9686f6b47cf77cb2fbde7ff14fdc
Matthew McDermott <mattmcdermott8@gmail.com>
2025-02-28 13:34:24 -0500

    Used the importable meds dataset fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,35 +6,16 @@ import tempfile
 from pathlib import Path
 
 import pytest
-from meds import code_metadata_filepath, subject_splits_filepath
 
-from .preprocessing import MEDS_CODE_METADATA, MEDS_SHARDS, PREPROCESS_SCRIPT, SPLITS_DF
-
-
-@pytest.fixture(scope="session")
-def MEDS_dataset() -> Path:
-    with tempfile.TemporaryDirectory() as data_dir:
-        data_dir = Path(data_dir)
-
-        for shard, df in MEDS_SHARDS.items():
-            fp = data_dir / f"data/{shard}.parquet"
-            fp.parent.mkdir(parents=True, exist_ok=True)
-            df.write_parquet(fp)
-
-        (data_dir / "metadata").mkdir(parents=True, exist_ok=True)
-
-        MEDS_CODE_METADATA.write_parquet(data_dir / code_metadata_filepath)
-        SPLITS_DF.write_parquet(data_dir / subject_splits_filepath)
-
-        yield data_dir
+from .preprocessing import PREPROCESS_SCRIPT
 
 
 @pytest.fixture(scope="session")
-def tensorized_MEDS_dataset(MEDS_dataset: Path) -> tuple[Path, Path]:
+def tensorized_MEDS_dataset(simple_static_MEDS: Path) -> tuple[Path, Path]:
     with tempfile.TemporaryDirectory() as cohort_dir:
         command = [
             str(PREPROCESS_SCRIPT),
-            f"MEDS_dataset_dir={str(MEDS_dataset)}",
+            f"MEDS_dataset_dir={str(simple_static_MEDS)}",
             f"output_dir={cohort_dir}",
         ]
 
@@ -48,4 +29,4 @@ def tensorized_MEDS_dataset(MEDS_dataset: Path) -> tuple[Path, Path]:
 
         assert out.returncode == 0, error_str
 
-        yield MEDS_dataset, Path(cohort_dir)
+        yield simple_static_MEDS, Path(cohort_dir)

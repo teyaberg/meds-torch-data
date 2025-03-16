7e380b505c36bfb3d279842e0ecc3ad07a450b40
Matthew McDermott <mattmcdermott8@gmail.com>
2025-03-16 19:14:57 -0400

    Fixed incorrect type hint

--- tests/preprocessing/test_preprocess.py
+++ tests/preprocessing/test_preprocess.py
@@ -12,7 +12,7 @@ from .test_tensorization import WANT_NRTS
 from .test_tokenization import WANT_SCHEMAS
 
 
-def test_preprocess(tensorized_MEDS_dataset: tuple[Path, Path]):
+def test_preprocess(tensorized_MEDS_dataset: Path):
     cohort_dir = tensorized_MEDS_dataset
 
     cohort_dir_contents = list(cohort_dir.rglob("*.parquet")) + list(cohort_dir.rglob("*.nrt"))
